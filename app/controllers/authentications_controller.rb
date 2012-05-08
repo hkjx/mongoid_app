e57c047dac6d5f66bb94dd51650ab494557df797
unknown <Admin@microsof-93a1e7.(none)>
2012-05-09 00:57:24 +0300

    second

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -15,7 +15,7 @@ private
 		auth = request.env["omniauth.auth"]
 		@authentication = Authentication.where(uid: auth["uid"].to_s, provider: auth["provider"]).first
 		if current_user
-			current_user.authentications.create!(:uid => auth["uid"], :provider => auth["provider"])
+			@authentication = current_user.authentications.create!(:uid => auth["uid"], :provider => auth["provider"])
 			session[:provider] = @authentication.provider
 			sign_in_and_redirect(:user, current_user)
 		elsif @authentication && !current_user
@@ -23,16 +23,8 @@ private
 			session[:provider] = @authentication.provider
 			sign_in_and_redirect(:user, user)
 		else
-			user = User.new
-			user.create_with_session(auth)
-			if user.save
-				user.authentications << @authentication
-				session[:provider] = @authentication.provider
-				sign_in_and_redirect(:user, user)
-			else
-				session[:omniauth] = auth.except("extra")
-				redirect_to new_user_registration_path
-			end
+			session[:omniauth] = auth.except("extra")
+			redirect_to new_user_registration_path
 		end
 	end
 end
